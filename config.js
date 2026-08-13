608c7ab33bb8a5ae54356b893a4b04f940606903
404Err4o4R <8kg4fnj9kg@privaterelay.appleid.com>
2026-08-13 18:03:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.PLAY_TOGETHER_CONFIG = {
-  API_BASE_URL: "https://404err404r--github-io-boardgame.mtcczyy4w9n.workers.dev"
+  API_BASE_URL: "https://play-together-game-server.playtogether-err4o4r.workers.dev"
 };
