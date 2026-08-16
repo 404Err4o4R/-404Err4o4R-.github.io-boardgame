2f298fa01d1794e15e3f956c74360d50065c6521
404Err4o4R <8kg4fnj9kg@privaterelay.appleid.com>
2026-08-16 17:02:12 +0800

    Delete boardgame_github_pages/play_together_cloudflare directory

--- boardgame_github_pages/play_together_cloudflare/config.js
+++
@@ -1,5 +0,0 @@
-window.PLAY_TOGETHER_CONFIG = {
-  // Replace this after deploying the Cloudflare Worker.
-  // Example: https://play-together-game-server.your-subdomain.workers.dev
-  API_BASE_URL: "https://404err404r--github-io-boardgame.playtogether-err4o4r.workers.dev"
-};
