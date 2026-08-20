98cac3078939a047ac3ddd0115b129c8a75a3ad7
404Err4o4R <8kg4fnj9kg@privaterelay.appleid.com>
2026-08-20 14:58:37 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
 window.PLAY_TOGETHER_CONFIG = {
-  // 換成你 wrangler deploy 之後嗰個 Worker 網址（例如：
-  // https://404err404r--github-io-boardgame.<你嘅-cloudflare-subdomain>.workers.dev）
-  WORKER_URL: "https://REPLACE-WITH-YOUR-WORKER-URL.workers.dev"
+  WORKER_URL: "https://404err4o4r--github-io-boardgame.8kg4fnj9kg.workers.dev"
 };
