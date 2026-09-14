8bbb4c1944a3b71e6082a9e1e6d9bc07444eedd6
404Err4o4R <8kg4fnj9kg@privaterelay.appleid.com>
2026-09-14 09:48:30 +0800

    Update loader.js

--- question-bank/loader.js
+++ question-bank/loader.js
@@ -1,3 +1,4 @@
 export { GAME1_QUESTIONS, GAME1_CATEGORIES } from "./game1.js";
 export { GAME2_QUESTIONS, GAME2_CATEGORIES } from "./game2.js";
 export { GAME4_QUESTIONS } from "./game4.js";
+export { GAME5_OPENERS } from "./game5.js";
