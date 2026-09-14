9b5024585f6d95ba54fe3868d4b49b55297f8daf
404Err4o4R <8kg4fnj9kg@privaterelay.appleid.com>
2026-09-14 09:48:09 +0800

    Add files via upload

---
+++ question-bank/game5.js
@@ -0,0 +1,662 @@
+export const GAME5_OPENERS = [
+  {
+    "id": 1,
+    "text": {
+      "yue": {
+        "question": "深夜十二點"
+      },
+      "zh": {
+        "question": "深夜十二點"
+      }
+    }
+  },
+  {
+    "id": 2,
+    "text": {
+      "yue": {
+        "question": "電梯突然停咗"
+      },
+      "zh": {
+        "question": "電梯突然停了"
+      }
+    }
+  },
+  {
+    "id": 3,
+    "text": {
+      "yue": {
+        "question": "門鈴響咗"
+      },
+      "zh": {
+        "question": "門鈴響了"
+      }
+    }
+  },
+  {
+    "id": 4,
+    "text": {
+      "yue": {
+        "question": "佢收到個訊息"
+      },
+      "zh": {
+        "question": "他收到一則訊息"
+      }
+    }
+  },
+  {
+    "id": 5,
+    "text": {
+      "yue": {
+        "question": "佢望一望手機"
+      },
+      "zh": {
+        "question": "他看了一眼手機"
+      }
+    }
+  },
+  {
+    "id": 6,
+    "text": {
+      "yue": {
+        "question": "屋企部電話突然響起"
+      },
+      "zh": {
+        "question": "家裡的電話突然響起"
+      }
+    }
+  },
+  {
+    "id": 7,
+    "text": {
+      "yue": {
+        "question": "嗰日落緊雨"
+      },
+      "zh": {
+        "question": "那天下著雨"
+      }
+    }
+  },
+  {
+    "id": 8,
+    "text": {
+      "yue": {
+        "question": "巴士遲遲未到"
+      },
+      "zh": {
+        "question": "公車遲遲不來"
+      }
+    }
+  },
+  {
+    "id": 9,
+    "text": {
+      "yue": {
+        "question": "佢打開個信箱"
+      },
+      "zh": {
+        "question": "他打開信箱"
+      }
+    }
+  },
+  {
+    "id": 10,
+    "text": {
+      "yue": {
+        "question": "佢揭開個箱"
+      },
+      "zh": {
+        "question": "他打開箱子"
+      }
+    }
+  },
+  {
+    "id": 11,
+    "text": {
+      "yue": {
+        "question": "窗外閃咗一下"
+      },
+      "zh": {
+        "question": "窗外閃了一下"
+      }
+    }
+  },
+  {
+    "id": 12,
+    "text": {
+      "yue": {
+        "question": "電話冇晒電"
+      },
+      "zh": {
+        "question": "手機沒電了"
+      }
+    }
+  },
+  {
+    "id": 13,
+    "text": {
+      "yue": {
+        "question": "佢企喺鏡前面"
+      },
+      "zh": {
+        "question": "他站在鏡子前面"
+      }
+    }
+  },
+  {
+    "id": 14,
+    "text": {
+      "yue": {
+        "question": "老師突然點名叫佢"
+      },
+      "zh": {
+        "question": "老師突然點名叫他"
+      }
+    }
+  },
+  {
+    "id": 15,
+    "text": {
+      "yue": {
+        "question": "會議開到一半停電"
+      },
+      "zh": {
+        "question": "會議開到一半停電"
+      }
+    }
+  },
+  {
+    "id": 16,
+    "text": {
+      "yue": {
+        "question": "佢見到陌生人揮手"
+      },
+      "zh": {
+        "question": "他看到陌生人揮手"
+      }
+    }
+  },
+  {
+    "id": 17,
+    "text": {
+      "yue": {
+        "question": "佢隻鞋著錯咗腳"
+      },
+      "zh": {
+        "question": "他的鞋穿錯腳了"
+      }
+    }
+  },
+  {
+    "id": 18,
+    "text": {
+      "yue": {
+        "question": "佢個外套袋有張紙條"
+      },
+      "zh": {
+        "question": "他的外套口袋有張紙條"
+      }
+    }
+  },
+  {
+    "id": 19,
+    "text": {
+      "yue": {
+        "question": "佢喺街執到個銀包"
+      },
+      "zh": {
+        "question": "他在街上撿到一個錢包"
+      }
+    }
+  },
+  {
+    "id": 20,
+    "text": {
+      "yue": {
+        "question": "佢個手機收到陌生來電"
+      },
+      "zh": {
+        "question": "他的手機收到陌生來電"
+      }
+    }
+  },
+  {
+    "id": 21,
+    "text": {
+      "yue": {
+        "question": "佢突然識飛"
+      },
+      "zh": {
+        "question": "他突然會飛"
+      }
+    }
+  },
+  {
+    "id": 22,
+    "text": {
+      "yue": {
+        "question": "天空突然變綠色"
+      },
+      "zh": {
+        "question": "天空突然變成綠色"
+      }
+    }
+  },
+  {
+    "id": 23,
+    "text": {
+      "yue": {
+        "question": "佢個影子郁咗一吓"
+      },
+      "zh": {
+        "question": "他的影子動了一下"
+      }
+    }
+  },
+  {
+    "id": 24,
+    "text": {
+      "yue": {
+        "question": "佢發現自己識讀心"
+      },
+      "zh": {
+        "question": "他發現自己會讀心"
+      }
+    }
+  },
+  {
+    "id": 25,
+    "text": {
+      "yue": {
+        "question": "佢瞓醒發現自己變咗細個"
+      },
+      "zh": {
+        "question": "他睡醒發現自己變小了"
+      }
+    }
+  },
+  {
+    "id": 26,
+    "text": {
+      "yue": {
+        "question": "佢收到一個心心表情"
+      },
+      "zh": {
+        "question": "他收到一個愛心表情"
+      }
+    }
+  },
+  {
+    "id": 27,
+    "text": {
+      "yue": {
+        "question": "前度突然send訊息"
+      },
+      "zh": {
+        "question": "前任突然傳訊息"
+      }
+    }
+  },
+  {
+    "id": 28,
+    "text": {
+      "yue": {
+        "question": "佢喺街見到前度"
+      },
+      "zh": {
+        "question": "他在街上遇到前任"
+      }
+    }
+  },
+  {
+    "id": 29,
+    "text": {
+      "yue": {
+        "question": "曖昧對象讚咗佢張相"
+      },
+      "zh": {
+        "question": "曖昧對象按讚他的照片"
+      }
+    }
+  },
+  {
+    "id": 30,
+    "text": {
+      "yue": {
+        "question": "佢收到一束花"
+      },
+      "zh": {
+        "question": "他收到一束花"
+      }
+    }
+  },
+  {
+    "id": 31,
+    "text": {
+      "yue": {
+        "question": "佢個手機震咗一下"
+      },
+      "zh": {
+        "question": "他的手機震了一下"
+      }
+    }
+  },
+  {
+    "id": 32,
+    "text": {
+      "yue": {
+        "question": "佢隔籬坐低一個人"
+      },
+      "zh": {
+        "question": "他旁邊坐下一個人"
+      }
+    }
+  },
+  {
+    "id": 33,
+    "text": {
+      "yue": {
+        "question": "佢打開個衣櫃"
+      },
+      "zh": {
+        "question": "他打開衣櫃"
+      }
+    }
+  },
+  {
+    "id": 34,
+    "text": {
+      "yue": {
+        "question": "佢聽到有人叫佢個名"
+      },
+      "zh": {
+        "question": "他聽到有人叫他的名字"
+      }
+    }
+  },
+  {
+    "id": 35,
+    "text": {
+      "yue": {
+        "question": "佢望向窗外"
+      },
+      "zh": {
+        "question": "他望向窗外"
+      }
+    }
+  },
+  {
+    "id": 36,
+    "text": {
+      "yue": {
+        "question": "佢收到一份包裹"
+      },
+      "zh": {
+        "question": "他收到一份包裹"
+      }
+    }
+  },
+  {
+    "id": 37,
+    "text": {
+      "yue": {
+        "question": "佢揭開本相簿"
+      },
+      "zh": {
+        "question": "他翻開一本相簿"
+      }
+    }
+  },
+  {
+    "id": 38,
+    "text": {
+      "yue": {
+        "question": "佢個電腦彈出個視窗"
+      },
+      "zh": {
+        "question": "他的電腦跳出一個視窗"
+      }
+    }
+  },
+  {
+    "id": 39,
+    "text": {
+      "yue": {
+        "question": "佢行到路口"
+      },
+      "zh": {
+        "question": "他走到路口"
+      }
+    }
+  },
+  {
+    "id": 40,
+    "text": {
+      "yue": {
+        "question": "佢摷咗摷個袋"
+      },
+      "zh": {
+        "question": "他翻了翻包包"
+      }
+    }
+  },
+  {
+    "id": 41,
+    "text": {
+      "yue": {
+        "question": "佢企喺天台"
+      },
+      "zh": {
+        "question": "他站在天台"
+      }
+    }
+  },
+  {
+    "id": 42,
+    "text": {
+      "yue": {
+        "question": "佢聽到樓梯有腳步聲"
+      },
+      "zh": {
+        "question": "他聽到樓梯有腳步聲"
+      }
+    }
+  },
+  {
+    "id": 43,
+    "text": {
+      "yue": {
+        "question": "佢打開個抽屜"
+      },
+      "zh": {
+        "question": "他打開抽屜"
+      }
+    }
+  },
+  {
+    "id": 44,
+    "text": {
+      "yue": {
+        "question": "佢望住個天花板"
+      },
+      "zh": {
+        "question": "他望著天花板"
+      }
+    }
+  },
+  {
+    "id": 45,
+    "text": {
+      "yue": {
+        "question": "佢個朋友忽然靜咗"
+      },
+      "zh": {
+        "question": "他的朋友忽然安靜了"
+      }
+    }
+  },
+  {
+    "id": 46,
+    "text": {
+      "yue": {
+        "question": "佢企喺月台"
+      },
+      "zh": {
+        "question": "他站在月台"
+      }
+    }
+  },
+  {
+    "id": 47,
+    "text": {
+      "yue": {
+        "question": "佢見到一個熟悉嘅背影"
+      },
+      "zh": {
+        "question": "他看到一個熟悉的背影"
+      }
+    }
+  },
+  {
+    "id": 48,
+    "text": {
+      "yue": {
+        "question": "佢揭開張紙條"
+      },
+      "zh": {
+        "question": "他打開一張紙條"
+      }
+    }
+  },
+  {
+    "id": 49,
+    "text": {
+      "yue": {
+        "question": "佢望向手錶"
+      },
+      "zh": {
+        "question": "他看了看手錶"
+      }
+    }
+  },
+  {
+    "id": 50,
+    "text": {
+      "yue": {
+        "question": "佢企喺門口"
+      },
+      "zh": {
+        "question": "他站在門口"
+      }
+    }
+  },
+  {
+    "id": 51,
+    "text": {
+      "yue": {
+        "question": "佢聽到窗口有聲響"
+      },
+      "zh": {
+        "question": "他聽到窗邊有聲響"
+      }
+    }
+  },
+  {
+    "id": 52,
+    "text": {
+      "yue": {
+        "question": "佢個雨傘唔見咗"
+      },
+      "zh": {
+        "question": "他的雨傘不見了"
+      }
+    }
+  },
+  {
+    "id": 53,
+    "text": {
+      "yue": {
+        "question": "佢望到牆上有幅畫"
+      },
+      "zh": {
+        "question": "他看到牆上有幅畫"
+      }
+    }
+  },
+  {
+    "id": 54,
+    "text": {
+      "yue": {
+        "question": "佢企喺天橋度"
+      },
+      "zh": {
+        "question": "他站在天橋上"
+      }
+    }
+  },
+  {
+    "id": 55,
+    "text": {
+      "yue": {
+        "question": "佢摸到袋入面有嘢"
+      },
+      "zh": {
+        "question": "他摸到口袋裡有東西"
+      }
+    }
+  },
+  {
+    "id": 56,
+    "text": {
+      "yue": {
+        "question": "佢聽到有人喺度講佢個名"
+      },
+      "zh": {
+        "question": "他聽到有人在說他的名字"
+      }
+    }
+  },
+  {
+    "id": 57,
+    "text": {
+      "yue": {
+        "question": "佢見到桌上有張便條"
+      },
+      "zh": {
+        "question": "他看到桌上有張便條"
+      }
+    }
+  },
+  {
+    "id": 58,
+    "text": {
+      "yue": {
+        "question": "佢望向天空"
+      },
+      "zh": {
+        "question": "他望向天空"
+      }
+    }
+  },
+  {
+    "id": 59,
+    "text": {
+      "yue": {
+        "question": "佢企喺樓梯口"
+      },
+      "zh": {
+        "question": "他站在樓梯口"
+      }
+    }
+  },
+  {
+    "id": 60,
+    "text": {
+      "yue": {
+        "question": "佢打開個舊盒"
+      },
+      "zh": {
+        "question": "他打開一個舊盒子"
+      }
+    }
+  }
+];
